62abcd008c7e9f5d004902441b67f60392674bc8
manuelbent <manuelbent@pm.me>
2026-04-28 16:39:01 +0200

    router: update to use ioc for dependency injection

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,19 +1,19 @@
 import { Router } from 'express'
-import container from '../config/ioc'
+import ioc from '../config/ioc'
 
 const router = Router()
 
 // api routes
-router.post('/api/moments/generate',
-    (req, res, next) => container.generateMomentRequestValidator.validate(req, res, next),
-    (req, res, next) => container.promptValidator.handle(req, res, next),
-    (req, res) => container.momentController.generate(req, res)
+router.post('/api/capture',
+    (req, res, next) => ioc.generateMomentRequestValidator.validate(req, res, next),
+    (req, res, next) => ioc.promptValidator.handle(req, res, next),
+    (req, res) => ioc.momentController.generate(req, res)
 )
 
 // healthcheck
-router.get('/healthcheck', container.systemController.healthcheck)
+router.get('/healthcheck', ioc.systemController.healthcheck)
 
 // 404
-router.use(container.systemController.notFound)
+router.use(ioc.systemController.notFound)
 
 export default router
