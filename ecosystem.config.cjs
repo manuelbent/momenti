da7b4b352577ea5a72201e1ca3316dbc766acbba
manuelbent <manuelbent@pm.me>
2026-04-29 10:46:55 +0200

    style: format ecosystem.config.cjs for consistency and readability

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,18 @@
 module.exports = {
-  apps: [
-    {
-      name: "momenti-server",
-      cwd: "./server",
-      script: "npm",
-      args: "run dev",
-      watch: false,
-    },
-    {
-      name: "momenti-client",
-      cwd: "./client",
-      script: "npm",
-      args: "run dev",
-      watch: false,
-    },
-  ],
-};
-
+    apps: [
+        {
+            name: 'momenti-server',
+            cwd: './server',
+            script: 'npm',
+            args: 'run dev',
+            watch: false,
+        },
+        {
+            name: 'momenti-client',
+            cwd: './client',
+            script: 'npm',
+            args: 'run dev',
+            watch: false,
+        },
+    ],
+}
