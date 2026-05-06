4757a57c71a0eec0130169839c7bc9b485281ae8
manuelbent <manuelbent@pm.me>
2026-05-06 13:49:39 +0200

    feat: simplify form structure in FormElement component

--- client/src/lib/components/primitives/FormElement.svelte
+++ client/src/lib/components/primitives/FormElement.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
     import { getContext } from 'svelte'
 
-    export let css:         string      = ''
-    export let inputCss:    string      = ''
-    export let buttonCss:   string      = ''
-    export let buttonLabel: string      = 'Send'
-    export let fields:      FormField[] = []
+    export let css: string = ''
+    export let inputCss: string = ''
+    export let buttonCss: string = ''
+    export let buttonLabel: string = 'Send'
+    export let fields: FormField[] = []
 
     const viewOnly = getContext<boolean>('viewOnly') ?? false
 
@@ -17,51 +17,49 @@
     }
 </script>
 
-{#if submitted}
-    <div style={css}>
-        <p style="font-style: italic;">Thank you!</p>
-    </div>
-{:else}
-    <form style={css} on:submit|preventDefault={handleSubmit}>
-        {#each fields as field}
-            {#if field.type === 'subject'}
-                <p>{field.text}</p>
 
-            {:else if field.type === 'radio'}
-                <fieldset>
-                    {#if field.label}<legend>{field.label}</legend>{/if}
-                    {#each field.options as opt}
-                        <label>
-                            <input
+<form style={css} on:submit|preventDefault={handleSubmit}>
+    {#each fields as field}
+        {#if field.type === 'subject'}
+            <p>{field.text}</p>
+
+        {:else if field.type === 'radio'}
+            <fieldset>
+                {#if field.label}
+                    <legend>{field.label}</legend>
+                {/if}
+                {#each field.options as opt}
+                    <label>
+                        <input
                                 type="radio"
                                 name={field.name}
                                 value={opt.value}
                                 bind:group={values[field.name]}
                                 required
-                            />
-                            {opt.label}
-                        </label>
-                    {/each}
-                </fieldset>
+                        />
+                        {opt.label}
+                    </label>
+                {/each}
+            </fieldset>
 
-            {:else if field.type === 'input'}
-                <label>
-                    {#if field.label}<span>{field.label}</span>{/if}
-                    <input
+        {:else if field.type === 'input'}
+            <label>
+                {#if field.label}<span>{field.label}</span>{/if}
+                <input
                         style={inputCss}
                         type="text"
                         name={field.name}
                         placeholder={field.placeholder ?? ''}
                         bind:value={values[field.name]}
                         required
-                    />
-                </label>
-            {/if}
-        {/each}
+                />
+            </label>
+        {/if}
+    {/each}
 
-        <button type="submit" style={buttonCss}>{buttonLabel}</button>
-    </form>
-{/if}
+    <button type="submit" style={buttonCss}>{buttonLabel}</button>
+</form>
+
 <style>
     fieldset {
         display: flex;
