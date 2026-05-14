aa32fc7156cc83bf7d6d44c3dde0313a7684a8f1
manuelbent <manuelbent@pm.me>
2026-05-14 18:58:25 +0200

    fix: correct version spelling

--- server/src/controllers/SystemController.ts
+++ server/src/controllers/SystemController.ts
@@ -11,7 +11,7 @@ export default class SystemController {
     public async healthcheck(_: Request, res: Response) {
         res.status(200).json({
             name: 'momenti',
-            version: '0.0.0-alfa',
+            version: '0.0.0-alpha',
             description: 'AI-powered page builder. Your moment, one message away.',
         })
     }
