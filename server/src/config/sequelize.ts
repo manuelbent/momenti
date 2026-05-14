8472a534457efb406fea2911b149d82b69845b0d
manuelbent <manuelbent@pm.me>
2026-05-14 12:06:36 +0200

    refactor: change module type to commonjs

--- server/src/config/sequelize.ts
+++ server/src/config/sequelize.ts
@@ -1,13 +1,9 @@
-import { fileURLToPath } from 'node:url'
 import path from 'node:path'
 import { Sequelize } from 'sequelize'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: path.join(__dirname, './../../../database/data/db.sqlite'),
+    storage: path.resolve(__dirname, './../../../database/data/db.sqlite'),
     logging: false,
 })
 
