9e980c4a2c64e36328df21b1f8bfe91b9606ab1b
manuelbent <manuelbent@pm.me>
2026-05-17 14:27:14 +0200

    feat: remove rate limiting middleware from IoC and routes

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -38,7 +38,6 @@ router.put('/moments/:id',
 )
 
 router.post('/moments/:slug/submissions',
-    (req, res, next) => ioc.rateLimitMiddleware.handle(req, res, next),
     (req, res, next) => ioc.submitFormDataRequestValidator.validate(req, res, next),
     (req, res) => ioc.formSubmissionController.store(req, res)
 )
@@ -51,14 +50,12 @@ router.get('/moments/:slug/submissions',
 
 // invite keys routes
 router.post('/invite-keys/validate',
-    (req, res, next) => ioc.rateLimitMiddleware.handle(req, res, next),
     (req, res, next) => ioc.validateInviteKeyRequestValidator.validate(req, res, next),
     (req, res) => ioc.inviteKeyController.validate(req, res)
 )
 
 // image upload route
 router.post('/images',
-    (req, res, next) => ioc.rateLimitMiddleware.handle(req, res, next),
     (req, res, next) => ioc.inviteKeyMiddleware.handle(req, res, next),
     (req, res, next) => ioc.uploadMiddleware.handle(req, res, next),
     (req, res) => ioc.imageController.upload(req, res)
