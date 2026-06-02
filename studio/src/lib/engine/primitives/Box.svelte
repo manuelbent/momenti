f49770ea54b2cb5381d12f537088a779220ceac1
manuelbent <manuelbent@pm.me>
2026-06-02 18:39:07 +0200

    feat: add promptable functionality to Box component

--- studio/src/lib/engine/primitives/Box.svelte
+++ studio/src/lib/engine/primitives/Box.svelte
@@ -5,6 +5,8 @@
     export let node: MomentNode
 
     const viewOnly = getContext<boolean>('viewOnly') ?? false
+    let isPromptable = false
+    const promptableVariants = ['hero', 'section']
 
     $: isHero = !viewOnly && node.variant === 'hero'
     $: isSelected = isHero && $selectedNode?.id === node.id
@@ -41,16 +43,47 @@
 
         selectNode({ id: node.id, type: 'box', deleteId: node.id })
     }
+
+    const onMouseOver = () => {
+        if (viewOnly) {
+            return
+        }
+
+        if (promptableVariants.includes(node.variant!)) {
+            isPromptable = true
+        }
+    }
+
+    const onMouseOut = () => {
+        isPromptable = false
+    }
 </script>
 
 <div id={node.id}
      style="{node.css}{isHero ? ';user-select:none' : ''}"
      data-nid={isHero ? node.id : undefined}
      class:momenti-selected={isSelected}
+     class:momenti-promptable={isPromptable}
      onmousedown={isHero ? handleMouseDown : undefined}
      onclick={isHero ? handleClick : undefined}
      onkeydown={() => {}}
+     onmouseenter={onMouseOver}
+     onmouseleave={onMouseOut}
+     onfocus={() => {}}
+     onblur={() => {}}
      role={isHero ? 'presentation' : undefined}
 >
     <slot/>
+    {#if isPromptable}
+        <button onclick={() => console.log(node)}
+                class="absolute bg-[#f0ede8] px-2 py-1 rounded-full bottom-6 right-6 text-[10px] text-[#0d0d0d] cursor-pointer">
+            <span class="font-serif text-sm">m</span>
+        </button>
+    {/if}
 </div>
+
+<style>
+    .momenti-promptable {
+        position: relative;
+    }
+</style>
